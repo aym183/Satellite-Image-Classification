02939c604da0605dfd27d12db01774bd7c1c139c
aym183 <ayman.ali1302@gmail.com>
2024-04-01 14:30:56 +0100

    Added holdout, k_fold and Stratified_k_fold validation

--- Functions/classifiers.py
+++ Functions/classifiers.py
@@ -1,6 +1,7 @@
 from sklearn.svm import SVC
 
-def svc_classifier(train_set_x, test_set_x, train_set_y, test_set_y, features, no_of_features):
+
+def svc_classifier_holdout(train_set_x, test_set_x, train_set_y, test_set_y, features, no_of_features):
     svc = SVC(kernel='rbf') # Why poly and how does it work
     selected_features = train_set_x[:, features[:no_of_features]]
     selected_test_features = test_set_x[:, features[:no_of_features]]
--- Functions/validation.py
+++ Functions/validation.py
@@ -1,8 +1,18 @@
-from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import cross_val_score, KFold, StratifiedKFold
 import numpy as np
 
-def cross_valdiation():
-    return 1
+def holdout_validation():
+    return "holdout"
 
-def k_fold_validation():
-    return 2
+def cross_valdiation():
+    svc_scores = cross_val_score(svc, selected_features, y_train, cv=10)
+    print(svc_scores.mean(), svc_scores.std())
+
+def k_fold_cross_validation(stratified):
+    if stratified == True:
+        # strat_k_fold_cross validation
+        # Stratification was used because it preserves the percentage of samples for each class.
+        print("True")
+    else:
+        # k_fold_cross validation
+        print("False")
