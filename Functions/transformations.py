9ed38ff187a17aed797a11d3eafa037ae57ca880
aym183 <ayman.ali1302@gmail.com>
2024-04-01 13:05:54 +0100

    Added standardisation

--- Functions/transformations.py
+++ Functions/transformations.py
@@ -1,11 +1,13 @@
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
 
+# Normalisation
 def normalise_min_max(train_set, test_set):
     min_max_scaler = MinMaxScaler(feature_range=(-1, 1)) # ------ Doing normalisation before/after pearson has no impact
     x_train_norm = min_max_scaler.fit_transform(train_set)
     x_test_norm = min_max_scaler.fit_transform(test_set)
     return x_train_norm, x_test_norm
 
+# Standardisation
 def normalise_std_scaler(train_set, test_set):
     standard_scaler = StandardScaler()
     x_train_norm = standard_scaler.fit_transform(train_set)
