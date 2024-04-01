7b9ee8bdc8c0b3a104f245cd64f3002e989da493
aym183 <ayman.ali1302@gmail.com>
2024-04-01 01:06:04 +0100

    Edited classifier file naming

--- Functions/classifiers.py
+++ Functions/classifiers.py
@@ -1,9 +1,9 @@
 from sklearn.svm import SVC
 
-def train_svc_10_features(train_set_x, test_set_x, train_set_y, test_set_y, features):
+def svc_classifier(train_set_x, test_set_x, train_set_y, test_set_y, features, no_of_features):
     svc = SVC(kernel='rbf') # Why poly and how does it work
-    selected_features = train_set_x[:, features[:10]]
-    selected_test_features = test_set_x[:, features[:10]]
+    selected_features = train_set_x[:, features[:no_of_features]]
+    selected_test_features = test_set_x[:, features[:no_of_features]]
 
     svc.fit(selected_features, train_set_y)
     svc.score(selected_test_features, test_set_y) 
