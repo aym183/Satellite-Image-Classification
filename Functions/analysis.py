b4d4611742812f9403bda21e0b0ddf13fe8db717
aym183 <ayman.ali1302@gmail.com>
2024-04-01 14:05:21 +0100

    Added analysis file for pearson correlation calculation

---
+++ Functions/analysis.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+def plot_correlation_heatmap(corr_matrix, top_features):
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f", xticklabels=['Feature {}'.format(i) for i in range(1, len(top_features)+1)], yticklabels=['Target Variable'])
+    plt.title('Pearson Correlation Heatmap')
+    plt.xlabel('Features')
+    plt.ylabel('Target Variable')
+    plt.show()
+
+    # return plt
