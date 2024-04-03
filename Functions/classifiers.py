820cfaf71d9f3b4ff92c14ad289f75cfee48b839
aym183 <ayman.ali1302@gmail.com>
2024-04-03 13:01:09 +0100

    Added MLP classifier code

--- Functions/classifiers.py
+++ Functions/classifiers.py
@@ -19,5 +19,20 @@ def svc_classifier(train_set_x, test_set_x, train_set_y, test_set_y, cross_valid
     elif cross_validation_type == "k_fold_strat":
         k_fold_cross_validation_strat(concatenated_array_x, concatenated_array_y, 10)
 
-def mlp_classifier():
-    mlp = MLPClassifier()
+def mlp_classifier(train_set_x, test_set_x, train_set_y, test_set_y, cross_validation_type):
+    mlp = mlp_classifier = MLPClassifier(hidden_layer_sizes=(100,), activation='relu', solver='adam', random_state=42)
+    mlp.fit(train_set_x, train_set_y)
+    y_pred = mlp.predict(test_set_x)
+    print(f"Testing Accuracy - {y_pred}")
+    concatenated_array_x = np.concatenate((train_set_x, test_set_x), axis=0)
+    concatenated_array_y = np.concatenate((train_set_y, test_set_y), axis=0)
+    
+    # -------- EDIT ACCORDING TO MLP --------
+    # if cross_validation_type == "holdout":
+    #     holdout_validation(mlp, train_set_x, test_set_x, train_set_y, test_set_y)
+    # elif cross_validation_type == "cv":
+    #     cross_validation(mlp, train_set_x, train_set_y, 10)
+    # elif cross_validation_type == "k_fold":
+    #     k_fold_valdiation(concatenated_array_x, concatenated_array_y, 10)
+    # elif cross_validation_type == "k_fold_strat":
+    #     k_fold_cross_validation_strat(concatenated_array_x, concatenated_array_y, 10)
