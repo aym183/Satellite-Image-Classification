3491b65568a22274f1116f5c34d96c13955b5107
aym183 <ayman.ali1302@gmail.com>
2024-04-03 12:49:28 +0100

    Added function to fetch datasets

--- Functions/pre_processing.py
+++ Functions/pre_processing.py
@@ -2,6 +2,24 @@ import numpy as np
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt 
 
+def fetch_datasets():
+    x_train = np.load("datasets/x_train.npy")
+    x_test = np.load("datasets/x_test.npy")
+    y_train = np.load("datasets/y_train.npy")
+    y_test = np.load("datasets/y_test.npy")
+
+    # print(x_train.shape)
+    # print(x_test.shape)
+    # print(y_train.shape)
+    # print(y_test.shape)
+
+    # All values float
+
+    # print(x_train) 512 features -> Numerical features - Features with values that are continuous on a scale, statistical, or integer-related
+    # print(np.unique(y_train)) # [0. 1. 2. 3. 4. 5. 6. 7. 8. 9.] -> 10 classes of possible outputs, and the reoccurences are just one of these values indicating a class 
+    return x_train, x_test, y_train, y_test
+
+    
 def find_missing_values(input: np.ndarray, array_name: str) -> str:
     missing_values_count = np.isnan(input).sum()
     return f"Missing Values in {array_name} -> {missing_values_count}"
