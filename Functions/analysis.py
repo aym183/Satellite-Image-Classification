94006140c2842c18bd879f8d9b15ac5b7f76074a
aym183 <ayman.ali1302@gmail.com>
2024-04-04 14:36:41 +0100

    Added code for roc and det curves

--- Functions/analysis.py
+++ Functions/analysis.py
@@ -3,7 +3,7 @@ For visualisations
 '''
 import matplotlib.pyplot as plt 
 import seaborn as sns
-from sklearn.metrics import ConfusionMatrixDisplay, precision_recall_curve, auc
+from sklearn.metrics import ConfusionMatrixDisplay, precision_recall_curve, auc, roc_curve, det_curve
 import numpy as np
 
 # Can we use sns!????
@@ -34,8 +34,30 @@ def plot_precision_recall_curve(y_true, y_pred):
     plt.legend(loc='best')
     plt.show()
 
-def plot_roc_curve():
-    return "roc"
+def plot_roc_curve(y_true, y_pred):
+    plt.figure(figsize=(6, 4))
+    for i in range(len(np.unique(y_true))):
+        fpr, tpr, _ = roc_curve((y_true == i).astype(int), y_pred[:, i])
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, lw=2, label='Class {} (AUC = {:.2f})'.format(i, roc_auc))
 
-def plot_det_curve():
-    return "det"
+    plt.plot([0, 1], [0, 1], color='gray', lw=1, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend(loc='best')
+    plt.show()
+
+def plot_det_curve(y_true, y_pred):
+    plt.figure(figsize=(6, 4))
+    for i in range(len(np.unique(y_true))):
+        fpr, fnr, _ = det_curve((y_true == i).astype(int), y_pred[:, i])
+        plt.plot(fpr, fnr, lw=2, label='Class {}'.format(i))
+
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('False Negative Rate')
+    plt.title('DET Curve')
+    plt.legend(loc='best')
+    plt.show()
