aee54618b0bf8a5b52ed3d6b61818a86c5e68907
aym183 <ayman.ali1302@gmail.com>
2024-04-06 16:45:12 +0100

    loosely justified why I would try undersampling

--- Functions/classifiers.py
+++ Functions/classifiers.py
@@ -25,7 +25,7 @@ def svc_classifier(train_set_x, test_set_x, train_set_y, test_set_y, cross_valid
     return svc
 
 def mlp_classifier(train_set_x, test_set_x, train_set_y, test_set_y, cross_validation_type):
-    mlp = MLPClassifier(hidden_layer_sizes=(100,), activation='tanh', solver='adam', alpha=0.05, random_state=42, shuffle=False) # Configuration derived from hyperparameter tuning
+    mlp = MLPClassifier(hidden_layer_sizes=(50, 100, 50), activation='tanh', solver='sgd', alpha=0.0001, random_state=42, shuffle=False) # Configuration derived from hyperparameter tuning
     mlp.fit(train_set_x, train_set_y)
     # y_pred = mlp.predict(test_set_x)
     concatenated_array_x = np.concatenate((train_set_x, test_set_x), axis=0)
--- Functions/pre_processing.py
+++ Functions/pre_processing.py
@@ -28,19 +28,16 @@ def find_negative_values(input: np.ndarray, array_name: str) -> str:
     negative_values_count = (input < 0).sum() 
     return f"Negative Values in {array_name} -> {negative_values_count}"
     
-# Reference GPT
-def plot_split_of_values(input_arrays):
+def plot_split_of_values(input_arrays, input_text):
     fig, axs = plt.subplots(1, len(input_arrays), figsize=(15, 4)) 
     fig.suptitle('Split of Values', fontsize=16)
     
     for i, input_array in enumerate(input_arrays):
         flattened_array = input_array.flatten()
         axs[i].hist(flattened_array, bins=50, color='blue', alpha=0.7)
-        axs[i].set_title(f'Array {i+1}')
+        axs[i].set_title(f'{input_text[i]}')
         axs[i].set_xlabel('Values')
         axs[i].set_ylabel('Frequency')
-        axs[i].grid(True)
     
     plt.tight_layout()
     plt.show()
-    
