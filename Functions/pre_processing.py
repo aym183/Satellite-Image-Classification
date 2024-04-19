b78bc354051d1436acd10f911858de4db7215c7f
aym183 <ayman.ali1302@gmail.com>
2024-04-19 19:23:47 +0100

    Removed checks for negative values

--- Functions/pre_processing.py
+++ Functions/pre_processing.py
@@ -8,25 +8,12 @@ def fetch_datasets():
     y_train = np.load("datasets/y_train.npy")
     y_test = np.load("datasets/y_test.npy")
 
-    # print(x_train.shape)
-    # print(x_test.shape)
-    # print(y_train.shape)
-    # print(y_test.shape)
-
-    # All values float
-
-    # print(x_train) 512 features -> Numerical features - Features with values that are continuous on a scale, statistical, or integer-related
-    # print(np.unique(y_train)) # [0. 1. 2. 3. 4. 5. 6. 7. 8. 9.] -> 10 classes of possible outputs, and the reoccurences are just one of these values indicating a class 
     return x_train, x_test, y_train, y_test
 
     
 def find_missing_values(input: np.ndarray, array_name: str) -> str:
     missing_values_count = np.isnan(input).sum()
     return f"Missing Values in {array_name} -> {missing_values_count}"
-
-def find_negative_values(input: np.ndarray, array_name: str) -> str:
-    negative_values_count = (input < 0).sum() 
-    return f"Negative Values in {array_name} -> {negative_values_count}"
     
 def plot_feature_split_of_values(input_arrays, input_text):
     fig, axs = plt.subplots(1, len(input_arrays), figsize=(15, 4)) 
