cfe21e040dc5b7753440668092a4c6b15eaf5f21
aym183 <ayman.ali1302@gmail.com>
2024-04-20 14:13:58 +0100

    Implemented training after variance threshold

--- Functions/feature_selection.py
+++ Functions/feature_selection.py
@@ -1,5 +1,21 @@
-from sklearn.feature_selection import r_regression, f_regression
+from sklearn.feature_selection import r_regression, f_regression, VarianceThreshold
 import numpy as np
+
+def calculate_variance_threshold(train_set_x, test_set_x):
+    variances = np.var(train_set_x, axis=0)
+    # average_variance = np.mean(variances)
+    std_dev = np.std(variances) # Due to the high variability of data, standard deviation was taken - 0.026167810885013097
+
+    threshold = std_dev
+    variance_calculator = VarianceThreshold(threshold)
+    variance_calculator.fit(train_set_x)
+    x_train_selected = variance_calculator.transform(train_set_x)
+    x_test_selected = variance_calculator.transform(test_set_x)
+
+    print(x_train_selected.shape)
+    print(x_test_selected.shape)
+    return x_train_selected, x_test_selected
+
 
 def pearson_correlation(train_set_x, train_set_y):
     pr_coeff = r_regression(train_set_x, train_set_y)
