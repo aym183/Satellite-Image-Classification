b9ce6d794bf210267f0b6911a99498a92b6c8c98
aym183 <ayman.ali1302@gmail.com>
2024-04-22 12:51:50 +0100

    Added variance threshold top 10 feature selection

--- Functions/feature_selection.py
+++ Functions/feature_selection.py
@@ -1,7 +1,8 @@
 from sklearn.feature_selection import r_regression, f_regression, VarianceThreshold
 import numpy as np
 
-def calculate_variance_threshold(train_set_x, test_set_x):
+# GPT refer
+def calculate_variance_threshold(train_set_x, test_set_x, top_10_features):
     variances = np.var(train_set_x, axis=0)
     average_variance = np.mean(variances) # To get the threshold value, the average of the variance for each feature was taken - 0.05
     # std_dev = np.std(variances) # Due to the high variability of data, standard deviation was taken - 0.026167810885013097
@@ -9,12 +10,27 @@ def calculate_variance_threshold(train_set_x, test_set_x):
     threshold = average_variance
     variance_calculator = VarianceThreshold(threshold)
     variance_calculator.fit(train_set_x)
-    x_train_selected = variance_calculator.transform(train_set_x)
-    x_test_selected = variance_calculator.transform(test_set_x)
+    # print(x_train_selected.shape)
+    # print(x_test_selected.shape)
 
-    print(x_train_selected.shape)
-    print(x_test_selected.shape)
-    return x_train_selected, x_test_selected
+    if top_10_features:
+        kept_features_idx = variance_calculator.get_support(indices=True)
+        kept_features_variance = np.var(train_set_x[:, kept_features_idx], axis=0)
+        sorted_indices = np.argsort(kept_features_variance)[::-1]
+        top_10_features_train = kept_features_idx[sorted_indices][:10]
+        top_10_variance_train = kept_features_variance[sorted_indices][:10]
+
+        X_test_selected = test_set_x[:, kept_features_idx]
+        kept_features_variance_test = np.var(X_test_selected, axis=0)
+        sorted_indices_test = np.argsort(kept_features_variance_test)[::-1]
+        top_10_features_test = kept_features_idx[sorted_indices_test][:10]
+        top_10_variance_test = kept_features_variance_test[sorted_indices_test][:10]
+
+        return top_10_features_train, top_10_features_test
+    else: 
+        x_train_selected = variance_calculator.transform(train_set_x)
+        x_test_selected = variance_calculator.transform(test_set_x)
+        return x_train_selected, x_test_selected
 
 
 def pearson_correlation(train_set_x, train_set_y):
@@ -26,3 +42,5 @@ def f_regression_scores(train_set_x, train_set_y):
     f_scores, p_value = f_regression(train_set_x, train_set_y)
     imp_features = np.argsort(np.abs(f_scores))
     return imp_features
+
+# def poisson_method():
