05b90429edb2e8fe008d2d42252d96f43d9d8606
aym183 <ayman.ali1302@gmail.com>
2024-05-01 10:21:34 +0100

    Added hyperparam config for mlp

--- functions/model.py
+++ functions/model.py
@@ -1,6 +1,6 @@
 import joblib
 import numpy as np
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, KFold
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 
@@ -37,8 +37,50 @@ def find_best_configuration_svc(parameters, train_set_x, test_set_x, train_set_y
     return {
         "parameters": {"c": selected_c, "kernel": selected_kernel, "gamma": selected_gamma},
         "training_accuracy": best_training,
-        "testing_accuracy": best_test,
+        "testing_accuracy": best_test
     }
 
 def find_best_configuration_mlp(parameters, train_set_x, test_set_x, train_set_y, test_set_y):
-    print("MLP")
+    selected_hidden_layers = 0 
+    selected_activation = ''
+    selected_solver = '' 
+    selected_alpha = '' 
+    best_training = 0
+    best_test = 0
+    outer_cv = KFold(n_splits=10, shuffle=True)
+
+    for layer in range(len(parameters["hidden_layers"])):
+        for activation in range(len(parameters["activations"])):
+            for solver in range(len(parameters["solvers"])):
+                for alpha in range(len(parameters["alphas"])):
+                        inner_scores = []
+                        mlp_clf = MLPClassifier(hidden_layer_sizes=parameters["hidden_layers"][layer], activation=parameters["activations"][activation], solver=parameters["solvers"][solver], alpha=parameters["alphas"][alpha])
+                        mlp_clf.fit(train_set_x, train_set_y)
+                        current_train = mlp_clf.score(train_set_x, train_set_y)
+                        current_test = mlp_clf.score(test_set_x, test_set_y)
+                        # Nested CV
+                        for train_index, val_index in outer_cv.split(train_set_x):
+                            X_train, X_val = train_set_x[train_index], train_set_x[val_index]
+                            Y_train, Y_val = train_set_y[train_index], train_set_y[val_index]
+                            mlp_clf.fit(X_train, Y_train)
+                            test_score = mlp_clf.fit(X_val, Y_val)
+                            inner_scores.append(test_score)
+                        
+                        mean_score = sum(inner_scores) / len(inner_scores)
+                        if (current_train > best_training) and (current_test > best_test):
+                            best_training = current_train
+                            best_test = current_test
+                            selected_hidden_layers = parameters["hidden_layers"][layer]
+                            selected_activation = parameters["activations"][activation]
+                            selected_solver = parameters["solvers"][solver]
+                            selected_alpha = parameters["alphas"][alpha]
+
+    return {
+        "parameters": {"hidden_layers": selected_hidden_layers, "activation": selected_activation, "solver": selected_solver, "alpha": selected_alpha},
+        "training_accuracy": best_training,
+        "testing_accuracy": best_test
+    }
+
+                        # print(f"------- With hidden_layers={hidden_layers[layer]}, activation={activations[activation]}, solver={solvers[solver]}, alpha={alphas[alpha]}")
+                        # holdout_validation(mlp_clf, x_train_norm, x_test_norm, y_train, y_test)
+                        # print(f"Mean - {mean_score}")
